b11584365a7e10ad6c7d60b70d32081fd42740db
murtaza <180080243+murtaza-bukhari@users.noreply.github.com>
2026-07-26 23:24:27 +0500

    bug fixes

--- src/data/checkpoints.js
+++ src/data/checkpoints.js
@@ -1,13 +1,13 @@
 const checkpoints = [
   { id: 1, row: 1, col: 19 },
-  { id: 1, row: 1, col: 7 },
-  { id: 2, row: 7, col: 6 },
-  { id: 3, row: 11, col: 9 },
-  { id: 4, row: 15, col: 4 },
-  { id: 5, row: 17, col: 10 },
-  { id: 6, row: 21, col: 1 },
-  { id: 7, row: 21, col: 15 },
-  { id: 8, row: 3, col: 18 }
+  { id: 2, row: 1, col: 7 },
+  { id: 3, row: 7, col: 6 },
+  { id: 4, row: 11, col: 9 },
+  { id: 5, row: 15, col: 4 },
+  { id: 6, row: 17, col: 10 },
+  { id: 7, row: 21, col: 1 },
+  { id: 8, row: 21, col: 15 },
+  { id: 9, row: 1, col: 17 }
 ];
 
 export default checkpoints;
